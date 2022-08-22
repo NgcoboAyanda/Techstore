ddf34858b9f1713c07c20cfc5a7dd2804e503927
Ayanda Ngcobo <ayanda13@protonmail.com>
2022-08-22 21:18:43 +0200

    removed cra bloat

---
+++ src/Base/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Pages
+
+const App = ()=>{
+
+    return(
+        <div className='app'>
+            <Router>
+                <Switch>
+                    <Route>
+
+                    </Route>
+                </Switch>
+            </Router>
+            <footer className='footer'>
+
+            </footer>
+        </div>
+    )
+}
+
+export default App;
